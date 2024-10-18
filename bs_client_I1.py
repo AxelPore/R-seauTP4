26b55fd94ed7a1f45fd86a731766c5938ff68bde
Axel Poré <axel.pore@gmail.com>
2024-10-18 16:31:37 +0200

    first commit

---
+++ bs_client_I1.py
@@ -0,0 +1,19 @@
+import socket
+import sys 
+
+host = "10.1.1.15"
+port = "13337"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+
+s.sendall("Meooooo !")
+
+data = s.recv(1024)
+
+print(f"Le serveur a répondu {repr(data)}")
+
+s.close()
+
+sys.exit()
---
+++ bs_server_I1.py
@@ -0,0 +1,29 @@
+import socket
+
+host = "10.1.1.15"
+port = "13337"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((host, port))
+
+s.listen(1)
+
+conn, addr = s.accept()
+
+while True :
+    
+    try :
+        
+        data = conn.recv(1024)
+        
+        if not data : break
+        
+        conn.sendall("Hi mate !")
+        
+        print(f"Données reçues du client : {data}")
+    
+    except socket.error:
+        
+        print("Error Occured.")
+        break
