db4f3525d244826882291e393ef472d95fd5bcdd
Axel Poré <axel.pore@gmail.com>
2024-10-24 15:40:43 +0200

    commit

---
+++ bs_client_I2.py
@@ -0,0 +1,28 @@
+import socket
+import sys 
+
+host = "10.1.2.17"
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+
+try :
+    s.sendall(b"Hello there")
+    print(f"Connecté avec succès au serveur {host} sur le port {port}")
+except :
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+
+message = input('Que veux-tu envoyer au serveur : ')
+message.encode('utf-8')
+s.sendall({message})
+
+data = s.recv(1024)
+
+print(f"Le serveur a répondu {repr(data)}")
+
+s.close()
+
+sys.exit()
