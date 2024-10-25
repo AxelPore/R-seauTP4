f6ef17f7dd89a9b1cd4dede360aabd65dd8d7f78
Axel Poré <axel.pore@gmail.com>
2024-10-25 16:54:24 +0200

    commit

--- bs_server_II1.py
+++ bs_server_II1.py
@@ -52,7 +52,7 @@ def host_recieve(host):
                 else :
                     compteur += 1
         print(compteur)            
-        if compteur != 0 :
+        if compteur > 0 :
             ValueError(f"ERROR -l argument invalide. L'adresse {host} n'est pas l'une des adresses IP de cette machine.")
             sys.exit(4)     
         else :
