f889bf58956b93310a2acc29141edfbb8db8faac
Axel Poré <axel.pore@gmail.com>
2024-10-30 17:19:27 +0100

    commit

--- bs_server_II2A.py
+++ bs_server_II2A.py
@@ -18,7 +18,7 @@ formatter = logging.Formatter(
     style="{",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
-
+logger.warning(f"hello {last_minute}")
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(add_help=False)
 
