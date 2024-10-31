38ff6c52e4edc1b0453f690132b93247c7d399c4
Axel Poré <axel.pore@gmail.com>
2024-10-31 14:36:16 +0100

    commit

--- bs_server_II2A.py
+++ bs_server_II2A.py
@@ -88,7 +88,7 @@ def server(host, port):
 
     while True :
         while addr == None :
-            
+            print(last_minute)
             if now.minute != last_minute :
                 last_minute = now.minute
                 logger.warning(f"Aucun client depuis plus de une minute.")
