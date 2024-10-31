e525142cae86b481996a1728a30f76ace2736f85
Axel Poré <axel.pore@gmail.com>
2024-10-31 15:27:36 +0100

    commit

--- bs_server_II2A.py
+++ bs_server_II2A.py
@@ -8,20 +8,40 @@ import datetime
 
 logger = logging.getLogger("logs")
 logger.setLevel(10)
-yellow = "\x1b[33;20m"
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 console_handler = logging.StreamHandler()
 file_handler = logging.FileHandler("/var/log/bs_server/bs_server.log", mode="a", encoding="utf-8")
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
-formatter = logging.Formatter(
-    logger.warning: yellow 
-    "{asctime}  {levelname}  {message}",
-    style="{",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+# formatter = logging.Formatter(
+    
+#     "{asctime}  {levelname}  {message}",
+#     style="{",
+#     datefmt="%Y-%m-%d %H:%M:%S",
+# )
 
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
+file_handler.setFormatter(CustomFormatter)
+console_handler.setFormatter(CustomFormatter)
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(add_help=False)
 
