70d072a51d1d403468f4892be418152d47a0a426
Axel Poré <axel.pore@gmail.com>
2024-11-21 17:58:18 +0100

    commit

---
+++ web_sync.py
@@ -0,0 +1,22 @@
+import argparse
+import requests
+
+def get_content(url):
+    page = requests.get(url)
+    return page
+    
+def write_content(page, file):
+    f = open(file, "a")
+    f.write(page)
+    f.close()
+
+def main():
+    file = "/tmp/web_page"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("web_sync.py", action="store")
+    args = parser.parse_args()
+    url = args
+    page = get_content(url)
+    write_content(page, file)
+    
+main()
