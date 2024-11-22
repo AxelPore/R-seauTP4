d1bcb914a8bc712fdbb2c2630d97753605ab9028
Axel Poré <axel.pore@gmail.com>
2024-11-22 16:55:37 +0100

    commit

---
+++ web_sync_multiple.py
@@ -0,0 +1,37 @@
+import argparse
+import sys
+import requests
+from os import path
+
+def get_content(url):
+    page = requests.get(url)
+    return page
+
+def write_content(page, file):
+    f = open(file, "a")
+    f.write(page)
+    f.close()
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-f", "--file", action="store")
+
+    args = parser.parse_args()
+
+    if not args.file:
+        print("You should specify a valid file with -f or --file.")
+        sys.exit(1)
+
+    urlfile = args.file
+    with open(urlfile, 'r') as f :
+        for line in f :
+            page = get_content(line).text
+            file = "/tmp/web_" + path.basename(line)
+            write_content(page, file)
+
+    
+    
+
+main()
