ef95ab1c9d0517b40bb38e77df1c12f2baf2de0a
AxelPore <axel.pore@ynov.com>
2024-12-10 19:49:08 +0100

    commit_tp6

---
+++ TP6/chat_client_ii_3.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+import aioconsole
+import asyncio
+
+
+
+async def asInput(r, w) :
+    while True:
+        lines = []
+        while True:
+            ZaLine = await aioconsole.ainput()
+            if not ZaLine:
+                    break
+            lines.append(ZaLine)
+        line = '\n'.join(lines)
+        if line == 'exit':
+            sys.exit(0)
+        w.write(line.encode())
+        await w.drain()
+
+
+async def asRecieve(r, w) :
+    while True:
+        data = await r.read(1024)
+        if not data:
+            break
+        print(f"{data.decode()}")
+
+async def main() :
+    reader, writer = await asyncio.open_connection(host="10.1.1.22", port=8888)
+    tasks = [asInput(reader, writer), asRecieve(reader, writer)]
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+sys.exit(0)
