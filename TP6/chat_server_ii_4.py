4b1dec045abeb48c83126a798b7230cf7b69d9d3
AxelPore <axel.pore@ynov.com>
2024-12-10 20:52:29 +0100

    commit_tp6

--- TP6/chat_server_ii_4.py
+++ TP6/chat_server_ii_4.py
@@ -17,13 +17,27 @@ async def handle_client_msg(reader, writer):
         message = data.decode()
         for addrs in CLIENTS.keys():
             if addrs[0] != addr[0]:
+                messList = message.split("\n")
                 IP = addr[0].replace("'", "")
-                CLIENTS[addrs]["w"].write(f"{IP}:{addr[1]} a dit : {message}".encode())
-                await CLIENTS[addrs]["w"].drain()
+                print(messList)
+                if len(messList) > 1:
+                    print("more than one")
+                    CLIENTS[addrs]['w'].write(f"{IP}:{addr[1]} a dit : {messList[0]}".encode())
+                    await CLIENTS[addrs]["w"].drain()
+                    spaces = " " * len(f'{IP}:{addr[1]}:> ')
+                    for line in messList[1:]:
+                        CLIENTS[addrs]['w'].write(b"\n")
+                        CLIENTS[addrs]['w'].write(f"{spaces} {line}".encode())
+                        await CLIENTS[addrs]["w"].drain()
+                else:
+                    print("only one")
+                    CLIENTS[addrs]["w"].write(f"{IP}:{addr[1]} a dit : {messList[0]}".encode())
+                    await CLIENTS[addrs]["w"].drain()
                 CLIENTS[addrs]['w'].write(b"\n")
                 await CLIENTS[addrs]["w"].drain()
                 print(f"Message received from {IP}:{addr[1]!r} : {message!r}")
-
+            else:
+                print("message not sent to self")
 
 async def main():
     server = await asyncio.start_server(handle_client_msg, '10.1.2.17', 13337)
