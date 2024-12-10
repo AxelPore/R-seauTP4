29fb5519ac7d42a760169f2193102388eeeaf4a9
AxelPore <axel.pore@ynov.com>
2024-12-10 20:23:17 +0100

    commit_tp6

---
+++ TP6/chat_server_ii_3.py
@@ -0,0 +1,30 @@
+import asyncio
+
+async def Server(reader, writer):
+    while True:
+        data = await reader.read(1024)
+        addr = writer.get_extra_info('peername')
+
+        if data == b'':
+            break
+
+        message = data.decode()
+        IP = addr[0].replace("'", "")
+        print(f"Message received from {IP}:{addr[1]!r} : {message!r}")
+        
+        writer.write(f"Hello {IP} : {addr[1]!r}".encode())
+
+        await writer.drain()
+        
+
+async def main():
+    server = await asyncio.start_server(Server, "10.1.2.17", 13337)
+
+    addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
+    print(f"Serving on  {addrs}")
+
+    async with server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
