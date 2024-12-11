f530497682da3edba9610388ad44b74799299f45
AxelPore <axel.pore@ynov.com>
2024-12-11 12:03:04 +0100

    commit_tp6

--- TP6/chat_server_ii_5.py
+++ TP6/chat_server_ii_5.py
@@ -1,4 +1,49 @@
 import asyncio
+import logging
+
+logger = logging.getLogger("logs")
+logger.setLevel(10)
+fmt = "%(levelname)8s"
+fmt2 = "%(asctime)s %(levelname)8s %(message)s"
+class CustomFormatter(logging.Formatter):
+
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
+    white = '\x1b[38;5;255m'
+
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: "%(asctime)s" + self.grey + self.fmt + self.reset + " %(message)s",
+            logging.INFO: "%(asctime)s" + self.white + self.fmt + self.reset + " %(message)s",
+            logging.WARNING: "%(asctime)s" + self.yellow + self.fmt + self.reset + " %(message)s",
+            logging.ERROR: "%(asctime)s" + self.red + self.fmt + self.reset + " %(message)s",
+            logging.CRITICAL: "%(asctime)s" + self.bold_red + self.fmt + self.reset + " %(message)s",
+            
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+        return formatter.format(record)
+    
+console_handler = logging.StreamHandler()
+console_handler.setLevel(10)
+console_handler.setFormatter(CustomFormatter(fmt))
+
+file_handler = logging.FileHandler("/var/log/bs_server/bs_server.log", mode="a", encoding="utf-8")
+file_handler.setLevel(10)
+file_handler.setFormatter(logging.Formatter(fmt2))
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
 global CLIENTS
 CLIENTS = {}
 
@@ -13,7 +58,7 @@ async def handle_client_msg(reader, writer):
         message = data.decode()
         if 'Hello|' in message :
             pseudo = message.split('|')[1]  
-            
+        logger.info(f"{pseudo} ({addr[0]}) s'est connecté.")
         CLIENTS[addr] = {}
         CLIENTS[addr]['w'] = writer
         CLIENTS[addr]['r'] = reader
@@ -32,14 +77,15 @@ async def handle_client_msg(reader, writer):
                 IP = addr[0].replace("'", "")
                 CLIENTS[addrs]["w"].write(f"{pseudo} a dit : {List[0]}".encode())
                 await CLIENTS[addrs]["w"].drain()
-                print(f"Message received from {pseudo} : {message!r}")
+                logger.info(f"Le client {pseudo} a envoyé \"{message}\".")
+                logger.info(f"Message envoyé aux autre utilisateur.")
 
 
 async def main():
     server = await asyncio.start_server(handle_client_msg, '10.1.2.17', 13337)
 
     addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
-    print(f'Serving on {addrs}')
+    logger.info(f'Serving on {addrs}')
 
     async with server:
         await server.serve_forever()
